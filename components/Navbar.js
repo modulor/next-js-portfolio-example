f4c338832f178d4e5ab869207c0210a70de5dd34
Mario Lopez <modulor@hotmail.es>
2022-08-04 10:41:37 -0500

    Navbar: import "Link"

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg bg-primary">
     <div className="container">
@@ -16,51 +18,18 @@ const Navbar = () => (
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
+            <Link href="/">
+              <a className="nav-link active" aria-current="page">
+                Home
+              </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled">Disabled</a>
+            <Link href="/blog">
+              <a className="nav-link">Blog</a>
+            </Link>
           </li>
         </ul>
       </div>
