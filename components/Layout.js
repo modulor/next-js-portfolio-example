f5b0f53d00689ac0c1c0ef283e425d39efce7ea6
Mario Lopez <modulor@hotmail.es>
2022-08-04 10:48:05 -0500

    Layout: add padding up and down

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 const Layout = ({ children }) => (
   <>
     <Navbar />
-    <div className="container">
+    <div className="container py-4">
       <div className="row">
         <div className="col-12">{children}</div>
       </div>
